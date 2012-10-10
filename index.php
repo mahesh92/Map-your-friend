02e1dcfc94fe2493aa33b80b37923d70b36c0751
Bharath Thiruveedula <bharath_ves@hotmail.com>
2012-10-10 21:35:33 +0530

    Displaying the all users location in map

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div id="nick">
+            <form method="GET" action="display.php">
+                <label for="name">Nick : </label>
+                <input type = "text" id="name" name="name"></input>
+                <input type="submit">
+             </form>
+        </div>
+    </body>
+</html>
