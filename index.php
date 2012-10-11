576f4ddc7c2510b0c60331d4e608c2a903486e9a
root <root@ubuntu.(none)>
2012-10-11 20:29:32 +0530

    made changes to css

--- index.php
+++ index.php
@@ -4,47 +4,10 @@
     <style>
 
 
-     body {
-                background: white; 
-                width: 600px; 
-                margin: 0px auto; 
-                padding: 0px auto;
-                /* background-image:url('BlankMap.png');
-                background-repeat:no-repeat; */
-               }
-    .nick{
-                margin-left:auto;
-                margin-right:auto;
-                border-style:solid;
-                width:400px;
-                height:250px;
-                padding:10px;
-                border:5px solid gray;
-                margin:10px;
-                right:0px;
-                background-image:url('res/img/myf.png');
-                background-repeat:no-repeat;
-                background-size: 420px 320px;
-                font-family:"Times New Roman", Times, serif;
-                font-weight:bold;
-                font-size:24px;
-                position : absolute;
-                top : 100px;
-                left : 450px;
-    }
-    form{
-     position : absolute;
-     top : 100px;
-     left : 50px; 
-    }
-    label
-    {
-        color : white;
-    }
-    </style
+    </style>
     </head>
     <body>
-    
+        <link rel="stylesheet" type="text/css" href="src/css/style.css">
             <div id="nick" class="nick">
             <center>
                 <form method="GET" action="src/php/map.php">
