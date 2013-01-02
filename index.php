9912e3f8e05507e4e621601b779db0aaa0b979ea
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-01-02 21:45:36 +0530

    changed the extension of the image

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../res/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../res/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="../res/ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="res/ico/favicon.png">
+    <link rel="shortcut icon" href="res/ico/favicon.ico">
   </head>
 
   <body>
