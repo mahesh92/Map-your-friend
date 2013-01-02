6bd144e915db51c2348f2a89ea4c27804a82b924
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-01-02 21:46:21 +0530

    Changed the title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>myf</title>
+    <title>Map Your Friend</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
