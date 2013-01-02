14d72622e5cbe56fdc6d8c8010b2c29cb5250971
A Abhiram <abhiram.ampabathina@gmail.com>
2013-01-02 19:32:25 +0530

    Fresh start with standards of git and orgs style...

--- index.php
+++ index.php
@@ -1,17 +1,59 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="src/css/style.css">
-    </head>
-    <body>
-            <div id="nick" class="nick">
-            <center>
-                <form method="POST" action="src/php/map.php">
-                    <label for="name">Nick Name: </label>
-                    <input type = "text" id="name" name="name"></input>
-                    <br>
-                    <center><input type="submit"></center>
-                </form></center>
-            </div>        
-    </body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>myf</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+ 
+    <script src="src/js/jquery.js"></script>
+    <script src="src/js/bootstrap-dropdown.js"></script>
+    <script src="src/js/script.js"></script>
+    
+    <link href="src/css/bootstrap.css" rel="stylesheet">
+    <link href="src/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="src/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../res/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../res/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../res/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../res/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="res/ico/favicon.png">
+  </head>
+
+  <body>
+    <div class="container-narrow">
+        <div class="masthead">
+            <img src="res/img/Black_and_white_223x75.png" class="img-rounded">
+            <ul class="nav nav-pills pull-right">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="">About the app</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Get Involved <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Contribute</a></li>
+                        <li><a href="#">Donate</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+      <hr>
+
+    <div class="jumbotron">
+        <h2>Stay connected always</h2>
+        <p class="lead">Connect to your friends and move with them. This app provides you and your friends to locate and communicate each other.</p>
+        <a class="btn btn-large btn-success" href="#">Sign up today</a>
+    </div>
+    <div class="footer">
+        <p>&copy; Company 2012</p>
+    </div> 
+    </div> <!-- /container -->
+  </body>
 </html>
