b36c99b0a2a64b2c0517fd186d93bcacd7056115
A Abhiram <abhiram.ampabathina@gmail.com>
2013-01-03 17:24:10 +0530

    made a bad bootstrap lightbox for a form, which is not responsive

--- index.php
+++ index.php
@@ -5,25 +5,26 @@
     <meta charset="utf-8">
     <title>Map Your Friend</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<META NAME="KEYWORDS" CONTENT="mapyourfriend,myf,how to locate my friend">
-<META NAME="DESCRIPTION" CONTENT="This app lets you locate your friends online and communicate to each other.">
-<META HTTP-EQUIV="CONTENT-LANGUAGE" CONTENT="EN">
-<META NAME="revisit-after" CONTENT="14 days">
-<META NAME="Generator" CONTENT="http://websitesubmit.hypermart.net/">
-<META NAME="robots" CONTENT="all">
-<META NAME="Author" CONTENT="Abhiram">
-<META NAME="Copyright" CONTENT="copyright">
+    <META NAME="KEYWORDS" CONTENT="mapyourfriend,myf,how to locate my friend">
+    <META NAME="DESCRIPTION" CONTENT="This app lets you locate your friends online and communicate to each other.">
+    <META HTTP-EQUIV="CONTENT-LANGUAGE" CONTENT="EN">
+    <META NAME="revisit-after" CONTENT="14 days">
+    <META NAME="robots" CONTENT="all">
+    <META NAME="Author" CONTENT="Abhiram,Bharath">
+    <META NAME="Copyright" CONTENT="copyright">
 <!-- MetaTags http://websitesubmit.hypermart.net/ -->
 <!-- This app lets you locate your friends online and communicate to each other. -->
  
     <script src="src/js/jquery.js"></script>
     <script src="src/js/bootstrap-dropdown.js"></script>
+    <script src="src/js/lightbox-form.js"></script>
     <script src="src/js/bootstrap-carousel.js"></script>
     <script src="src/js/script.js"></script>
-    
+    <script src="src/js/bootstrap-lightbox.js"></script>
     <link href="src/css/bootstrap.css" rel="stylesheet">
     <link href="src/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="src/css/style.css" rel="stylesheet">
+    <link href="src/css/bootstrap-lightbox.css" rel="stylesheet">
     <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.ico">
 
     <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.png">
@@ -31,6 +32,13 @@
   </head>
 
   <body>
+
+
+
+
+
+
+
     <div class="container-narrow">
         <div class="masthead">
             <img src="res/img/Black_and_white_223x75.png" class="img-rounded">
@@ -46,11 +54,28 @@
                 </li>
             </ul>
 
-        </div>
-      <hr>
-    <div class="">
+<div id="demoLightbox" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="lightbox-header">
+        <button type="button" class="close" data-dismiss="lightbox" aria-hidden="true">&times;</button>
 
     </div>
+    <div class="lightbox-content">
+ <form method="GET" action="lightbox-form-test.html" target="_parent">
+    
+    <p>Email adress: 
+      <input type="text" name="email" value="myself@somedomainname.com" maxlength="60" size="60">
+    </p>
+     
+    <p> 
+      <input type="submit" name="submit">
+      <input type="button" name="cancel" value="Cancel">
+    </p>
+  </form>    </div>
+</div>
+
+        </div>
+      <hr>
+
 <div id="myCarousel" class="carousel slide">
   <!-- Carousel items -->
   <div class="carousel-inner">
@@ -66,7 +91,7 @@
 
     <div class="jumbotron">
         <p class="lead"></p>
-        <button class="btn btn-large btn-success">Create a group</button>
+        <button class="btn btn-large btn-success" data-toggle="lightbox" href="#demoLightbox"">Create a group</button>
         <button class="btn btn-large btn">Join a Group</button>
     </div>
     <div class="footer">
--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,4 @@ $(document).ready(function () {
 }); 
 
 $('.carousel').carousel()
+$('#myLightbox').lightbox(options)
