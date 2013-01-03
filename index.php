e5fd52f819abd2aad95bb604af5e5664936b5795
A Abhiram <abhiram.ampabathina@gmail.com>
2013-01-03 21:26:40 +0530

    made bootstrap modal, and responsive too , removed bootstrap lightbox

--- index.php
+++ index.php
@@ -1,103 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Map Your Friend</title>
-    <meta charset="utf-8">
-    <title>Map Your Friend</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <META NAME="KEYWORDS" CONTENT="mapyourfriend,myf,how to locate my friend">
-    <META NAME="DESCRIPTION" CONTENT="This app lets you locate your friends online and communicate to each other.">
-    <META HTTP-EQUIV="CONTENT-LANGUAGE" CONTENT="EN">
-    <META NAME="revisit-after" CONTENT="14 days">
-    <META NAME="robots" CONTENT="all">
-    <META NAME="Author" CONTENT="Abhiram,Bharath">
-    <META NAME="Copyright" CONTENT="copyright">
-<!-- MetaTags http://websitesubmit.hypermart.net/ -->
-<!-- This app lets you locate your friends online and communicate to each other. -->
+    <head>
+        <title>Map Your Friend</title>
+        <meta charset="utf-8">
+        <title>Map Your Friend</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <META NAME="KEYWORDS" CONTENT="mapyourfriend,myf,how to locate my friend">
+        <META NAME="DESCRIPTION" CONTENT="This app lets you locate your friends online and communicate to each other.">
+        <META HTTP-EQUIV="CONTENT-LANGUAGE" CONTENT="EN">
+        <META NAME="revisit-after" CONTENT="14 days">
+        <META NAME="Generator" CONTENT="http://websitesubmit.hypermart.net/">
+        <META NAME="robots" CONTENT="all">
+        <META NAME="Author" CONTENT="Abhiram">
+        <META NAME="Author" CONTENT="Bharath">
+        <META NAME="Copyright" CONTENT="copyright">
+        <!-- MetaTags http://websitesubmit.hypermart.net/ -->
+        <!-- This app lets you locate your friends online and communicate to each other. -->
  
-    <script src="src/js/jquery.js"></script>
-    <script src="src/js/bootstrap-dropdown.js"></script>
-    <script src="src/js/lightbox-form.js"></script>
-    <script src="src/js/bootstrap-carousel.js"></script>
-    <script src="src/js/script.js"></script>
-    <script src="src/js/bootstrap-lightbox.js"></script>
-    <link href="src/css/bootstrap.css" rel="stylesheet">
-    <link href="src/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="src/css/style.css" rel="stylesheet">
-    <link href="src/css/bootstrap-lightbox.css" rel="stylesheet">
-    <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.ico">
-
-    <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.png">
-
-  </head>
-
-  <body>
-
-
-
-
-
-
-
-    <div class="container-narrow">
-        <div class="masthead">
-            <img src="res/img/Black_and_white_223x75.png" class="img-rounded">
-            <ul class="nav nav-pills pull-right">
-                <li class=""><a href="#">Home</a></li>
-                <li><a href="">About app</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Get Involved <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Feature request</a></li>
-                        <li><a href="#">Donate</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-<div id="demoLightbox" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="lightbox-header">
-        <button type="button" class="close" data-dismiss="lightbox" aria-hidden="true">&times;</button>
-
-    </div>
-    <div class="lightbox-content">
- <form method="GET" action="lightbox-form-test.html" target="_parent">
+        <script src="src/js/jquery.js"></script>
+        <script src="src/js/bootstrap-dropdown.js"></script>
+        <script src="src/js/bootstrap-carousel.js"></script>
+        <script src="src/js/bootstrap-transition.js"></script>
+        <script src="src/js/script.js"></script>
+        <script src="src/js/bootstrap-modal.js"></script>
     
-    <p>Email adress: 
-      <input type="text" name="email" value="myself@somedomainname.com" maxlength="60" size="60">
-    </p>
-     
-    <p> 
-      <input type="submit" name="submit">
-      <input type="button" name="cancel" value="Cancel">
-    </p>
-  </form>    </div>
-</div>
+        <link href="src/css/bootstrap.css" rel="stylesheet">
+        <link href="src/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="src/css/style.css" rel="stylesheet">
+        <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.ico">
+        <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.png">
 
-        </div>
-      <hr>
+    </head>
 
-<div id="myCarousel" class="carousel slide">
-  <!-- Carousel items -->
-  <div class="carousel-inner">
-    <div class="active item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
-    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
-    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-02.jpg"></div>
-  </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-</div>
+    <body>
+        <div class="container-narrow">
+            <div class="masthead">
+                <img src="res/img/Black_and_white_223x75.png" class="img-rounded">
+                <ul class="nav nav-pills pull-right">
+                    <li class=""><a href="#">Home</a></li>
+                    <li><a href="">About app</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Get Involved <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Feature request</a></li>
+                            <li><a href="#">Donate</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
 
-
-    <div class="jumbotron">
-        <p class="lead"></p>
-        <button class="btn btn-large btn-success" data-toggle="lightbox" href="#demoLightbox"">Create a group</button>
-        <button class="btn btn-large btn">Join a Group</button>
-    </div>
-    <div class="footer">
-            <p>&copy; Company 2012</p>
-    </div> 
-    </div> <!-- /container -->
-  </body>
+ 
+            <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h3 id="myModalLabel">Modal header</h3>
+                </div>
+                <div class="modal-body">
+                    <p>One fine body…</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                    <button class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+            <div id="myCarousel" class="carousel slide">
+            <!-- Carousel items -->
+                <div class="carousel-inner">
+                    <div class="active item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
+                    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
+                    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-02.jpg"></div>
+                </div>
+                <!-- Carousel nav -->
+                <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+            </div>
+            <div class="jumbotron">
+                <p class="lead"></p>
+                <button class="btn btn-large btn-success" data-toggle="modal" data-target="#myModal">Create a group</button>
+                <button class="btn btn-large btn">Join a Group</button>
+            </div>
+            <div class="footer">
+                <p>&copy; Company 2012</p>
+            </div> 
+        </div> <!-- /container -->
+    </body>
 </html>
     
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {  
     $('.dropdown-toggle').dropdown();  
+    $('.carousel').carousel('cycle');
+    $('.myModal').modal('toggle');
 }); 
 
-$('.carousel').carousel()
-$('#myLightbox').lightbox(options)
