0ead032c504a38c2f4ebb8af0ae88cdc41b34a34
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-01-03 17:30:22 +0530

    Fixed the sliding issue in carousel and cleaned te code

--- index.php
+++ index.php
@@ -1,78 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Map Your Friend</title>
-    <meta charset="utf-8">
-    <title>Map Your Friend</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<META NAME="KEYWORDS" CONTENT="mapyourfriend,myf,how to locate my friend">
-<META NAME="DESCRIPTION" CONTENT="This app lets you locate your friends online and communicate to each other.">
-<META HTTP-EQUIV="CONTENT-LANGUAGE" CONTENT="EN">
-<META NAME="revisit-after" CONTENT="14 days">
-<META NAME="Generator" CONTENT="http://websitesubmit.hypermart.net/">
-<META NAME="robots" CONTENT="all">
-<META NAME="Author" CONTENT="Abhiram">
-<META NAME="Copyright" CONTENT="copyright">
-<!-- MetaTags http://websitesubmit.hypermart.net/ -->
-<!-- This app lets you locate your friends online and communicate to each other. -->
+    <head>
+        <title>Map Your Friend</title>
+        <meta charset="utf-8">
+        <title>Map Your Friend</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <META NAME="KEYWORDS" CONTENT="mapyourfriend,myf,how to locate my friend">
+        <META NAME="DESCRIPTION" CONTENT="This app lets you locate your friends online and communicate to each other.">
+        <META HTTP-EQUIV="CONTENT-LANGUAGE" CONTENT="EN">
+        <META NAME="revisit-after" CONTENT="14 days">
+        <META NAME="Generator" CONTENT="http://websitesubmit.hypermart.net/">
+        <META NAME="robots" CONTENT="all">
+        <META NAME="Author" CONTENT="Abhiram">
+        <META NAME="Author" CONTENT="Bharath">
+        <META NAME="Copyright" CONTENT="copyright">
+        <!-- MetaTags http://websitesubmit.hypermart.net/ -->
+        <!-- This app lets you locate your friends online and communicate to each other. -->
  
-    <script src="src/js/jquery.js"></script>
-    <script src="src/js/bootstrap-dropdown.js"></script>
-    <script src="src/js/bootstrap-carousel.js"></script>
-    <script src="src/js/script.js"></script>
+        <script src="src/js/jquery.js"></script>
+        <script src="src/js/bootstrap-dropdown.js"></script>
+        <script src="src/js/bootstrap-carousel.js"></script>
+        <script src="src/js/bootstrap-transition.js"></script>
+        <script src="src/js/script.js"></script>
     
-    <link href="src/css/bootstrap.css" rel="stylesheet">
-    <link href="src/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="src/css/style.css" rel="stylesheet">
-    <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.ico">
+        <link href="src/css/bootstrap.css" rel="stylesheet">
+        <link href="src/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="src/css/style.css" rel="stylesheet">
+        <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.ico">
+        <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.png">
 
-    <link rel="shortcut icon" href="res/ico/new_logo_favicon_2.png">
+    </head>
 
-  </head>
-
-  <body>
-    <div class="container-narrow">
-        <div class="masthead">
-            <img src="res/img/Black_and_white_223x75.png" class="img-rounded">
-            <ul class="nav nav-pills pull-right">
-                <li class=""><a href="#">Home</a></li>
-                <li><a href="">About app</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Get Involved <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Feature request</a></li>
-                        <li><a href="#">Donate</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-        </div>
-      <hr>
-    <div class="">
-
-    </div>
-<div id="myCarousel" class="carousel slide">
-  <!-- Carousel items -->
-  <div class="carousel-inner">
-    <div class="active item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
-    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
-    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-02.jpg"></div>
-  </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-</div>
-
-
-    <div class="jumbotron">
-        <p class="lead"></p>
-        <button class="btn btn-large btn-success">Create a group</button>
-        <button class="btn btn-large btn">Join a Group</button>
-    </div>
-    <div class="footer">
-            <p>&copy; Company 2012</p>
-    </div> 
-    </div> <!-- /container -->
-  </body>
+    <body>
+        <div class="container-narrow">
+            <div class="masthead">
+                <img src="res/img/Black_and_white_223x75.png" class="img-rounded">
+                <ul class="nav nav-pills pull-right">
+                    <li class=""><a href="#">Home</a></li>
+                    <li><a href="">About app</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Get Involved <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Feature request</a></li>
+                            <li><a href="#">Donate</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div id="myCarousel" class="carousel slide">
+            <!-- Carousel items -->
+                <div class="carousel-inner">
+                    <div class="active item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
+                    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-01.jpg"></div>
+                    <div class="item"><img src="res/img/bootstrap-mdo-sfmoma-02.jpg"></div>
+                </div>
+                <!-- Carousel nav -->
+                <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+            </div>
+            <div class="jumbotron">
+                <p class="lead"></p>
+                <button class="btn btn-large btn-success">Create a group</button>
+                <button class="btn btn-large btn">Join a Group</button>
+            </div>
+            <div class="footer">
+                <p>&copy; Company 2012</p>
+            </div> 
+        </div> <!-- /container -->
+    </body>
 </html>
     
