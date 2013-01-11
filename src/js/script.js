3694402deb1a1d770f2f9d08789208f089ab8033
A Abhiram <abhiram.ampabathina@gmail.com>
2013-01-11 21:00:13 +0530

    made master and dev equivalent

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {  
     $('.dropdown-toggle').dropdown();  
     $('.carousel').carousel('cycle');
-    $('.myModal').modal('toggle');
+    $('.dialogBox').modal('toggle');
 }); 
 
