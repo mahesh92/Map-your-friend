41a4d939dd537bc563f0f24333db980f4ca870f8
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-01-18 22:40:56 +0530

    Added the mail feature

--- src/php/create.php
+++ src/php/create.php
@@ -7,15 +7,18 @@ if(isset($_POST['nickname']) && isset($_POST['email']) && isset($_POST['group'])
     $rand='';
     $data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     do{
-      $rand = '';
-      for($i=0;$i<10;$i++){
-	$rand = $rand.$data[rand(0,61)];
-      }
-      $group_id = $group.$rand;
-      $result = mysql_query("select * from users where group_id='".$group_id."'");
-      $row = mysql_num_rows($result);
+        $rand = '';
+        for($i=0;$i<10;$i++){
+	    $rand = $rand.$data[rand(0,61)];
+        }
+        $group_id = $group.$rand;
+        $result = mysql_query("select * from users where group_id='".$group_id."'");
+        $row = mysql_num_rows($result);
     }while($row!=0);
     $result = mysql_query("INSERT INTO `MYF`.`users` (`group_id`, `group_name`, `name`,`email`, `latitude`, `longitude`) VALUES ('".$group_id."','".$group."','".$nickname."','".$email."', '0', '0')");
+    $message = "Thank you registering in Map your friend.\nYour details are :\nNickname:".$nickname."\nGroup Name:".$group."\nGroup ID:".$group_id."";
+    $subject = "Thankyou for registering into Map your friend";
+    mail($email,$subject,$message,"From:Map Your Friend Team<no-reply@myf.org>");
     if(!$result){
         echo "Failed";
     }
