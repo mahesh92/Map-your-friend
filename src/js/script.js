125032b0bfae27c9ef4bd472ce6f27be1879029f
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-01-18 20:59:02 +0530

    Added the ajax  function to create group

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,21 @@ $(document).ready(function () {
     $('.dropdown-toggle').dropdown();  
     $('.carousel').carousel('cycle');
     $('.dialogBox').modal('toggle');    
+    $("input[type='submit']").click(function(){ 
+	var nickname = $('#nickname').val();
+	var group = $('#group').val();
+	var email = $('#email').val();
+	$.ajax({
+	    type : "POST",
+	    url : "http://localhost/src/php/create.php",
+	    data : {
+                nickname : nickname,
+		group : group,
+		email : email
+            },
+	    success : function(data){
+	    }
+	});
+	return false; 
+    });
 });
