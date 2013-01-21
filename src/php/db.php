aeb4a915433c42f117d923ba4f1c74b172fa38b3
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-01-21 22:11:30 +0530

    Solving the issue of inserting details of user's location into the databas

--- src/php/db.php
+++ src/php/db.php
@@ -1,19 +1,19 @@
 <?php
 include('database.php');
-    if(isset($_GET['groupid'])&&($_GET['name'])&&isset($_GET['lat'])&&isset($_GET['lng']))
+if(isset($_GET['groupid'])&&($_GET['name'])&&isset($_GET['lat'])&&isset($_GET['lng']))
+{
+    mysql_query("UPDATE `users` SET  `latitude` = '".$_GET['lat']."' , `longitude` = '".$_GET['lng']."' WHERE `group_id`='".$_GET['groupid']."' AND `name` = '".$_GET['name']."' ");
+    $query = "SELECT * FROM `users` where group_id = '".$_GET['groupid']."' ";
+    $to_encode = array(); 
+    $result=mysql_query($query);
+    while($row = mysql_fetch_assoc($result))
     {
-        mysql_query("UPDATE `users` SET  `latitude` = '".$_GET['lat']."' , `longitude` = '".$_GET['lng']."' WHERE `group_name`='".$_GET['groupid']."' AND `name` = '".$_GET['name']."' ");
-        $query = "SELECT * FROM `users` where group_name = '".$_GET['groupid']."' ";
-        $to_encode = array(); 
-        $result=mysql_query($query);
-        while($row = mysql_fetch_assoc($result))
-        {
-            $to_encode[] = $row;
-        }
-        echo json_encode($to_encode);
+        $to_encode[] = $row;
     }
-    else if(isset($_GET['name'])&&!isset($_GET['lat'])&&!isset($_GET['lng']))
-    {
-     //   mysql_query("DELETE from users where group_name = '".$_GET['groupid']."' and name='".$_GET['name']."' ");
+    echo json_encode($to_encode);
+}
+else if(isset($_GET['name'])&&!isset($_GET['lat'])&&!isset($_GET['lng']))
+{
+    //   mysql_query("DELETE from users where group_name = '".$_GET['groupid']."' and name='".$_GET['name']."' ");
     }
 ?>
